325c9c1e81dbc79f34347812615377a179feff95
gconsi21 <grantconsi@gmail.com>
2025-01-30 17:27:00 -0500

    Debgugging API lead

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("MongoDB Connected");
+    console.log("MongoDB Connected ${mongoose.connection.name}");
     const PORT = process.env.PORT || 5500;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => {
