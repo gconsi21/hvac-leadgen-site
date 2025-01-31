09134b92ad5c9f06ce12fffe85294354f4e5a72c
gconsi21 <grantconsi@gmail.com>
2025-01-30 20:34:26 -0500

    fixed express rate limit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const corsOptions = {
