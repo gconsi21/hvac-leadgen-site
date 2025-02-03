d7233dad94f189583ca8b4ead35d61d669dc4e3d
gconsi21 <grantconsi@gmail.com>
2025-02-02 19:06:57 -0500

    Working on cors. testing deploy

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.options("*", (req, res) => {
+app.options('*', (req, res) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, x-api-key");
